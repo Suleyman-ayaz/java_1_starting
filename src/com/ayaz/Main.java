204909f836d5bc40638f1a317d70a5751bd6d985
Suleyman Ayaz <suleymanyz@gmail.com>
2023-08-10 00:18:44 +0300

    Main updated

--- src/com/ayaz/Main.java
+++ src/com/ayaz/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		for(int i=0;i<20;i++) {
 		System.out.println(i+". Test");
-		System.out.println();
+		System.out.println("fasdfa");
 		}
 
 	}
